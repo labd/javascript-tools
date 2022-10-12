b22921ab16d2a7cb1490db933526702c096718d2
Boris Besemer <borisbesemer@gmail.com>
2022-10-12 14:20:24 +0200

    feat(eslint-config-next): add package
    Add package with defaults based on our projects, some configuration is still needed
    for individual projects to define root directories for next projects.

---
+++ packages/eslint-config-next/index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: ['error', 'info', 'warn'],
+      },
+    ],
+  },
+}
