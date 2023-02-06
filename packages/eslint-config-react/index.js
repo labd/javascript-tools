f31860a58f1a228ec2cab89d2d92e9ace726051a
Mitchell Slager <mslager92@gmail.com>
2023-02-06 17:15:42 +0100

    feat(eslint-config-react): add package (#4)
    * feat: add eslint config for React applications
    
    * feat: add @babel/eslint-parser
    
    * fix: remove js files from ignorePatterns
    
    * chore: update lockfile

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,68 @@
+const path = require('path')
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  plugins: ['import', 'jest', 'react', 'react-hooks'],
+
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+    project: path.resolve('tsconfig.json'),
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  extends: ['prettier', 'plugin:jest/recommended'],
+
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+        project: path.resolve('tsconfig.json'),
+        tsconfigRootDir: __dirname,
+        warnOnUnsupportedTypeScriptVersion: true,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-namespace': 'error',
+      },
+    },
+  ],
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+
+  ignorePatterns: ['*.d.ts', 'node_modules/', '*.generated.ts'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-require-imports': ['error'],
+    'no-duplicate-imports': ['error'],
+    'react/require-render-return': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+  },
+}
